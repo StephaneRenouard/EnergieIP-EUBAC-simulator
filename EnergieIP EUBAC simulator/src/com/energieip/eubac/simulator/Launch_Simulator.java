63d7afa419908946af34eba0402057faadd4bca9
StephaneRenouard <stephane.renouard@gmail.com>
2018-02-10 09:11:26 +0100

    update launcher

---
+++ EnergieIP EUBAC simulator/src/com/energieip/eubac/simulator/Launch_Simulator.java
@@ -0,0 +1,30 @@
+package com.energieip.eubac.simulator;
+
+/**
+ * this class is the main entry point for launching simulator
+ * @author stef
+ *
+ */
+public class Launch_Simulator {
+
+	/**
+	 * default main entry point
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		new Launch_Simulator();
+
+	}
+	
+	
+	/**
+	 * Default constructor
+	 */
+	public Launch_Simulator() {
+		
+		
+		
+	}
+
+}
