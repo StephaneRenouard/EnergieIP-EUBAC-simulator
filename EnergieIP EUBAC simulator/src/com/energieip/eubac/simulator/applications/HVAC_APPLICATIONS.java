2a21895098f1595a8f9685bbe86a85a17e9751ee
StephaneRenouard <stephane.renouard@gmail.com>
2018-02-10 09:11:35 +0100

    add applications

---
+++ EnergieIP EUBAC simulator/src/com/energieip/eubac/simulator/applications/HVAC_APPLICATIONS.java
@@ -0,0 +1,15 @@
+package com.energieip.eubac.simulator.applications;
+
+public class HVAC_APPLICATIONS {
+	
+	// no application, in case you don't want any regulation
+	public static final String No_application = "No_Application";
+	
+	// from euBAC
+	public static final String Radiator_heating = "Radiator_heating";
+	public static final String Fan_coil_unit_system_4_pipes="Fan_coil_unit_system_4_pipes";
+	public static final String Ceiling_system_heating_cooling="Ceiling_system_heating_cooling";
+	
+	
+
+}
