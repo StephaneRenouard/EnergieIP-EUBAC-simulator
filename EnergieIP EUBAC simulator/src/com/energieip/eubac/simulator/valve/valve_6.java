5b313a6d8b6a6c818c4696e8ad43384e67ecc386
stephanerenouard <stephane.renouard@gmail.com>
2018-02-16 10:59:36 +0100

    add valve_6 class

---
+++ EnergieIP EUBAC simulator/src/com/energieip/eubac/simulator/valve/valve_6.java
@@ -0,0 +1,13 @@
+package com.energieip.eubac.simulator.valve;
+
+public class valve_6 {
+	
+	// values are for 0-10V regulator, in %
+	static final int VALVE_HOT_100 = 97;
+	static final int VALVE_HOT_0 = 70;
+	
+	static final int VALVE_COLD_100 = 20;
+	static final int VALVE_COLD_0 = 20;
+	
+
+}
