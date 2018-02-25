e70b90010701a538a666e47598886918ce98200c
StephaneRenouard <stephane.renouard@gmail.com>
2018-02-25 18:11:03 +0100

    add a HVAC_VALVES class

---
+++ EnergieIP EUBAC simulator/src/com/energieip/eubac/simulator/valves/HVAC_VALVES.java
@@ -0,0 +1,11 @@
+package com.energieip.eubac.simulator.valves;
+
+public class HVAC_VALVES {
+
+	// no application, in case you don't want any regulation
+	public static final String No_application = "No_Application";
+	
+	// from euBAC
+	public static final String Valve_6 = "Valve_6";
+	
+}
